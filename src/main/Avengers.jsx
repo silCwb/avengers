093ae0b1eabf4f9ab6eb526065845fef6af304e7
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-25 20:06:51 -0300

    cmponente botão e css

--- src/main/Avengers.jsx
+++ src/main/Avengers.jsx
@@ -1,16 +1,17 @@
 import React, {Component} from "react"
 import './Avengers.css'
+import Button from '../components/Button'
 
 export default class Avengers extends Component {
     render() {
         return(
             <div className='avengers'>
                 <h1 className='avengers_title'> Avengers: Infinity War </h1>
-                <p className='avengers_subtitle' > 85% </p>
-                <p className='avengers_subtitles' > 156 minutes </p>
-                <text className='text'>An unprecedented cinematic journey ten years in the making and spanning the entire Marvel Cinematic Universe, Marvel Studios' "Avengers: Infinity War" brings to the screen the ultimate, deadliest showdown of all time. The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</text>
+                <p1 className='avengers_subtitle' > 85% </p1>
+                <p2 className='avengers_subtitles' > 156 minutes </p2>
+                <Button label='Check Review'/>
+                 <footer className='text'>An unprecedented cinematic journey ten years in the making and spanning the entire Marvel Cinematic Universe, Marvel Studios' "Avengers: Infinity War" brings to the screen the ultimate, deadliest showdown of all time. The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</footer>
             </div>
-            
         )
     }
 }
