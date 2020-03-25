fdcc92f2b387a2f35fb8bef245bf88867517b618
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-25 20:21:46 -0300

    added imagem

--- src/main/Avengers.jsx
+++ src/main/Avengers.jsx
@@ -7,11 +7,12 @@ export default class Avengers extends Component {
         return(
             <div className='avengers'>
                 <h1 className='avengers_title'> Avengers: Infinity War </h1>
-                <p1 className='avengers_subtitle' > 85% </p1>
-                <p2 className='avengers_subtitles' > 156 minutes </p2>
-                <Button label='Check Review'/>
-                 <footer className='text'>An unprecedented cinematic journey ten years in the making and spanning the entire Marvel Cinematic Universe, Marvel Studios' "Avengers: Infinity War" brings to the screen the ultimate, deadliest showdown of all time. The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</footer>
-            </div>
+                    <p1 className='avengers_subtitle' > 85% </p1> 
+                    <p2 className='avengers_subtitles' > 156 minutes </p2>
+                    <img classname='image' src="https://i.ibb.co/9tLcR7s/avengers.jpg" ></img>
+                     <Button label='Check Review'/>
+             <footer className='text'>An unprecedented cinematic journey ten years in the making and spanning the entire Marvel Cinematic Universe, Marvel Studios' "Avengers: Infinity War" brings to the screen the ultimate, deadliest showdown of all time. The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</footer>
+          /></div>
         )
     }
 }
