7da9367710c35bf4752d90ce5aed32c2d2c7a8d2
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-25 18:44:03 -0300

    criação dos componentes e arquivos css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from 'react'
 import ReactDom from 'react-dom'
+import './index.css'
+import Avengers from './main/Avengers'
+
+ReactDom.render(
+    <div>
+          <Avengers/>
+    </div>
+    , document.getElementById('root' ));
