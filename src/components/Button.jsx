89ff7c99cf8a46413232dfcae02c314e43123691
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-26 00:35:48 -0300

    new components added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import './Button.css'
 
 export default props =>
-    <button className='button'> {props.label} </button>
+    <button className='button' onClick= "https://www.rottentomatoes.com/m/avengers_infinity_war" target="_blank" > {props.label} </button>
--- src/main/Avengers.jsx
+++ src/main/Avengers.jsx
@@ -1,19 +1,21 @@
 import React, {Component} from "react"
 import './Avengers.css'
 import Button from '../components/Button'
-import Image from "../../src/images/avgrs.png"
+import Image from "../images/avg.png"
+import Logo from "../images/tom.png"
 
 export default class Avengers extends Component {
     render() {
         return(
             <div className='avengers'>
                 <h1 className='avengers_title'> Avengers: Infinity War </h1>
-                    <p1 className='avengers_subtitle' > 85% </p1> 
-                    <p2 className='avengers_subtitles' > 156 minutes </p2>
-                    <img src={Image} alt="logo" />
-                     <Button label='Check Review'/>
+                 <img classname='logo' src={Logo} alt="" />
+                 <p1 className='avengers_subtitle'>85%</p1> 
+                 <p2 className='avengers_subtitles' > 156 minutes </p2>
+                 <img classname='image' src={Image} alt="avg" />
+                <Button onclick="href = 'http://pt.stackoverflow.com'" label='Check Review'/>
              <footer className='text'>An unprecedented cinematic journey ten years in the making and spanning the entire Marvel Cinematic Universe, Marvel Studios' "Avengers: Infinity War" brings to the screen the ultimate, deadliest showdown of all time. The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</footer>
-          /></div>
+          </div>
         )
     }
 }
