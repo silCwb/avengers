b10562682ec5b8926fe6d91bed495e7c37080d4d
Silvana Siqueira <silvana.siqueira@gmail.com>
2020-03-26 11:24:40 -0300

    work is done ;)

--- src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import './Button.css'
-
-export default props =>
-    <button className='button' onClick= "https://www.rottentomatoes.com/m/avengers_infinity_war" target="_blank" > {props.label} </button>
--- src/main/Avengers.jsx
+++ src/main/Avengers.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react"
+import { Link } from 'react-router';
 import './Avengers.css'
-import Button from '../components/Button'
-import Image from "../images/avg.png"
+import Image from "../images/avengers.png"
 import Logo from "../images/tom.png"
 
 export default class Avengers extends Component {
@@ -9,11 +9,13 @@ export default class Avengers extends Component {
         return(
             <div className='avengers'>
                 <h1 className='avengers_title'> Avengers: Infinity War </h1>
-                 <img classname='logo' src={Logo} alt="" />
-                 <p1 className='avengers_subtitle'>85%</p1> 
-                 <p2 className='avengers_subtitles' > 156 minutes </p2>
-                 <img classname='image' src={Image} alt="avg" />
-                <Button onclick="href = 'http://pt.stackoverflow.com'" label='Check Review'/>
+                    <div className='percent'>
+                             <label className='avengers_subtitle'>85%</label> 
+                             <img classname='logo' src={Logo} alt="tomato" /> 
+                     </div>
+                             <p2 className='avengers_subtitles' > 156 minutes </p2>
+                        <img classname='image' src={Image} alt="avg" />
+                     <Link classname='link' href="https://www.rottentomatoes.com/m/avengers_infinity_war" target="_blank" > Check review </Link>
              <footer className='text'>An unprecedented cinematic journey ten years in the making and spanning the entire Marvel Cinematic Universe, Marvel Studios' "Avengers: Infinity War" brings to the screen the ultimate, deadliest showdown of all time. The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</footer>
           </div>
         )
